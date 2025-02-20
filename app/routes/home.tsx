101e6805145ce4b550bbb1931d07d6a9a5baab36
Wishnie, Jeff <jwishnie@gatech.edu>
2025-02-20 10:32:50 -0800

    add redis, why not

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,28 @@
-import { BattleBoks } from '../battleboks/battleboks'
+import { BattleBoks } from '~/battleboks/battleboks'
 import type { Route } from './+types/home'
 
-import { list as listChix } from '@vercel/blob'
+import { list as listChix, type ListBlobResultBlob } from '@vercel/blob'
+import { kv } from '@vercel/kv'
 import _sampleSize from 'lodash-es/sampleSize'
 import { useEffect } from 'react'
 import adjectives from '~/adjectives'
 import titles from '~/titles'
 
+const CHIX_KEY = 'chix'
+
+const getChix = async function () {
+  const fromStore = (await kv.get(CHIX_KEY)) as ListBlobResultBlob[] | null
+  if (!fromStore) {
+    const chix = (await listChix()).blobs
+    await kv.set(CHIX_KEY, chix, { ex: 3600 })
+    return chix
+  }
+
+  return fromStore
+}
+
 export async function loader({ params }: Route.LoaderArgs) {
-  const chix = _sampleSize((await listChix()).blobs, 2)
+  const chix = _sampleSize(await getChix(), 2)
   return {
     title: _sampleSize(titles)[0],
     adjective: _sampleSize(adjectives)[0],
