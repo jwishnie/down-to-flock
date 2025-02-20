0432f208e0afc7858eaaf8ff75a32a85d57dce0b
Wishnie, Jeff <jwishnie@gatech.edu>
2025-02-19 23:00:55 -0800

    display chicks

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
-import type { Route } from "./+types/home";
-import { BattleBoks } from "../battleboks/battleboks";
+import { BattleBoks } from '../battleboks/battleboks'
+import type { Route } from './+types/home'
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+import { list as listChix } from '@vercel/blob'
+import _sampleSize from 'lodash-es/sampleSize'
+import { useEffect } from 'react'
+import adjectives from '~/adjectives'
+import titles from '~/titles'
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const chix = _sampleSize((await listChix()).blobs, 2)
+  return {
+    title: _sampleSize(titles)[0],
+    adjective: _sampleSize(adjectives)[0],
+    left: chix[0],
+    right: chix[1],
+  }
 }
 
-export default function Home() {
-  return <BattleBoks />;
+export default function Home({
+  loaderData: { title, adjective, left, right },
+}: Route.ComponentProps) {
+  useEffect(() => {
+    document.title = title
+  })
+  return <BattleBoks adjective={adjective} left={left} right={right} />
 }
