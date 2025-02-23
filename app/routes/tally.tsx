a9fed122d460f04fc5665f00ddf59a9908fcd292
Wishnie, Jeff <jwishnie@gatech.edu>
2025-02-23 10:43:33 -0800

    paging working

--- app/routes/tally.tsx
+++ app/routes/tally.tsx
@@ -1,12 +1,13 @@
 import _range from 'lodash-es/range'
 import { useState } from 'react'
+import { redirect, useNavigate } from 'react-router'
 import { db, VOTES_TABLE } from '~/utils/data'
 import { safeParseInt } from '~/utils/general'
 import type { Route } from './+types/tally'
 
 const ROWS_PER_PAGE = 20
 
-export async function loader({ params: page }: Route.LoaderArgs) {
+export async function loader({ params: { page } }: Route.LoaderArgs) {
   const votes = await db
     .selectFrom(VOTES_TABLE)
     .selectAll()
@@ -15,34 +16,50 @@ export async function loader({ params: page }: Route.LoaderArgs) {
     .execute()
 
   const numPages = votes.length / ROWS_PER_PAGE
-  console.log(votes.length)
-  const current = safeParseInt(page) ?? 0
-  return { votes, numPages, currentPage: current < numPages ? current : 0 }
+  const current = safeParseInt(page)
+  if (!current || current > numPages) {
+    return redirect('/results/1')
+  }
+
+  return { votes, numPages, currentPage: current }
 }
 
 export default function Tally({
-  loaderData: { votes, numPages },
+  loaderData: { votes, numPages, currentPage },
 }: Route.ComponentProps) {
-  const goToPage = function (page: number) {
-    console.log(`goto page: ${page}`)
-  }
-  const [page, setPage] = useState(0)
-  const pager = _range(numPages).map((n, idx) => (
+  const nav = useNavigate()
+  const [page, setPage] = useState(currentPage)
+  const pager = _range(1, numPages + 1).map((n, idx) => (
     <span key={idx}>
       {!!idx ? ' | ' : ''}
-      <a onClick={() => goToPage(n)}>{n+1}</a>
+      {n == currentPage ? (
+        n
+      ) : (
+        <a
+          className="cursor-pointer"
+          onClick={() => {
+            setPage(n)
+          }}
+        >
+          {n}
+        </a>
+      )}
     </span>
   ))
-  const voteItems = votes.map(({ adjective, left, right, left_wins, id }) => (
-    <div
-      key={id}
-      className="flex items-center justify-center gap-x-5 py-6 w-full"
-    >
-      <img className="max-w-32" src={left_wins ? left : right} />
-      <div className="w-fit text-center min-w-70">is more {adjective} than</div>
-      <img className="max-w-32" src={left_wins ? right : left} />
-    </div>
-  ))
+  const voteItems = votes
+    .slice(ROWS_PER_PAGE * (page - 1), ROWS_PER_PAGE * page)
+    .map(({ adjective, left, right, left_wins, id }) => (
+      <div
+        key={id}
+        className="flex items-center justify-center gap-x-5 py-6 w-full"
+      >
+        <img className="max-w-32" src={left_wins ? left : right} />
+        <div className="w-fit text-center min-w-70">
+          is more {adjective} than
+        </div>
+        <img className="max-w-32" src={left_wins ? right : left} />
+      </div>
+    ))
   return (
     <div className="px-2">
       <div className="flex items-center justify-center py-16">Results</div>
