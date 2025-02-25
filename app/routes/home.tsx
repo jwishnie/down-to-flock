032e60ddbc09611473be3733dda9ed1d1f85637f
Wishnie, Jeff <jwishnie@gatech.edu>
2025-02-24 21:30:35 -0800

    fix 0s 1s index errors

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,28 +3,52 @@ import type { Route } from './+types/home'
 
 import _range from 'lodash-es/range'
 import _sampleSize from 'lodash-es/sampleSize'
-import _toInteger from 'lodash-es/toInteger'
 import { useEffect } from 'react'
 import { redirect, useLocation, useSubmit } from 'react-router'
 import adjectives from '~/adjectives'
 import titles from '~/titles'
-import { db, getChix, VOTES_TABLE, type ChickMeta } from '~/utils/data'
+import { db, getChix, VOTES_TABLE } from '~/utils/data'
+import { safeParseInt } from '~/utils/general'
 
+// return values are 1s based
+const pickYourBattle = async function (numChix: number) {
+  const [leftIndex, rightIndex] = _sampleSize(_range(1, numChix + 1), 2)
+  const adjective = _sampleSize(adjectives, 1)[0].toLocaleLowerCase()
+
+  return { adjective, leftIndex, rightIndex }
+}
+
+// params are 1s based
 export async function loader({
   params: { adjective, left, right },
 }: Route.LoaderArgs) {
+  const [left1sIndex, right1sIndex] = [left, right].map((e) => safeParseInt(e))
   const chix = await getChix()
+  const numChix = chix.length
 
-  const leftChick: ChickMeta | undefined = chix[parseInt(left!)]
-  const rightChick: ChickMeta | undefined = chix[parseInt(right!)]
-  if (!(adjective && left && right && leftChick && rightChick)) {
-    const adj = _sampleSize(adjectives)[0].toLocaleLowerCase()
-    const [leftI, rightI] = _sampleSize(_range(chix.length - 1), 2)
-    return redirect(`/${adj}/${leftI}/${rightI}`)
+  if (
+    !(
+      adjective &&
+      left1sIndex &&
+      right1sIndex &&
+      left1sIndex > 0 &&
+      right1sIndex > 0 &&
+      left1sIndex <= numChix &&
+      right1sIndex <= numChix
+    )
+  ) {
+    // invalid params, pick a new battle and redirect
+    const { adjective, leftIndex, rightIndex } = await pickYourBattle(numChix)
+    return redirect(`/${adjective}/${leftIndex}/${rightIndex}`)
   }
 
+  // pick the chix, convert 1s based from param to 0s based
+  const [leftChick, rightChick] = [left1sIndex, right1sIndex].map(
+    (i) => chix[i - 1]
+  )
+
   return {
-    title: _sampleSize(titles)[0],
+    title: _sampleSize(titles, 1)[0],
     adjective: adjective,
     left: leftChick,
     right: rightChick,
@@ -39,17 +63,37 @@ export async function action({
   const vote = formData.get('vote')
   const chix = await getChix()
 
+  const [leftChick, rightChick] = [left, right].map((i) => {
+    const index = safeParseInt(i)
+    if (!index) {
+      throw Response.json(
+        { message: 'Invlalid Chicken Index' },
+        {
+          status: 400,
+          statusText: 'Bad Request',
+        }
+      )
+    }
+    return chix[index - 1]
+  })
+
   await db
     .insertInto(VOTES_TABLE)
     .values({
       adjective,
-      left: chix[_toInteger(left)].src,
-      right: chix[_toInteger(right)].src,
+      left: leftChick.src,
+      right: rightChick.src,
       left_wins: vote === 'l',
     } as any)
     .execute()
 
-  return redirect('/')
+  // pick next battle
+  const {
+    adjective: newAdjective,
+    leftIndex,
+    rightIndex,
+  } = await pickYourBattle(chix.length)
+  return redirect(`/${newAdjective}/${leftIndex}/${rightIndex}`)
 }
 
 export default function Home({
@@ -62,7 +106,7 @@ export default function Home({
   const submit = useSubmit()
   const path = useLocation().pathname
   const vote = function (v: boolean) {
-    submit({ v }, { action: `${path}`, method: 'post' })
+    submit({ vote: v ? 'l' : 'r' }, { action: `${path}`, method: 'post' })
   }
   return (
     <BattleBoks
