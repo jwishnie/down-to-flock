a5074c528fb7ba6a43bb5edc9b0d9d91cfa4633a
Wishnie, Jeff <jwishnie@gatech.edu>
2025-02-28 12:54:55 -0800

    node env check

--- app/routes.ts
+++ app/routes.ts
@@ -2,9 +2,11 @@ import { type RouteConfig, prefix, route } from '@react-router/dev/routes'
 
 export default [
   route('/results/:page?', 'routes/tally.tsx'),
-  ...prefix('db', [
-    route('up', 'routes/db/up.tsx'),
-    route('down', 'routes/db/down.tsx'),
-  ]),
+  ...(process.env.NODE_ENV !== 'production'
+    ? prefix('db', [
+        route('up', 'routes/db/up.tsx'),
+        route('down', 'routes/db/down.tsx'),
+      ])
+    : []),
   route('/:adjective?/:left?/:right?/:vote?', 'routes/home.tsx'),
 ] satisfies RouteConfig
